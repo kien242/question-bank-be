ce6724a9afbf1481cba6ffa039bde47010dd7dee
kien242-Desktop <kien242tran@gmail.com>
2023-12-19 16:51:52 +0700

    update

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import { app } from './src/app';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const PORT: any = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Starting server with port', PORT);
 });
