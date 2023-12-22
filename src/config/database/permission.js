6fa2ddb80b1ac5a544fb9bb9457c2521284135d8
kien242-Desktop <kien242tran@gmail.com>
2023-12-23 00:15:06 +0700

    Add question Model

--- src/config/database/permission.js
+++ src/config/database/permission.js
@@ -1,9 +1,9 @@
 // Chỉ dành cho câu hỏi, hoắc bài kiểm tra
 
-const PERMISSIONS = {
+const ACCESS_AUTHORIZATION = {
   PUBLIC: 1111, // Bài kiểm tra, câu hỏi có thể truy cập bởi bất kỳ ai
   PROTECTED: 2222, // Bài kiểm tra, câu hỏi chỉ có thể truy cập bởi người dùng được lựa chọn
   VIA_LINK: 3333, // Bài kiểm tra, câu hỏi có thể truy cập thông qua baaft kỳ người nào có Link bài kiểm tra, câu hỏi đó
 };
 
-module.exports = { PERMISSIONS };
+module.exports = { ACCESS_AUTHORIZATION };
---
+++ src/model/question/model.js
@@ -0,0 +1,104 @@
+const { COLLECTION_NAME } = require('#config/database/collectionName.js');
+const { ACCESS_AUTHORIZATION } = require('#config/database/permission.js');
+const { QUESTION_TYPE } = require('#config/database/questionType.js');
+const { Schema, Types, model } = require('mongoose');
+
+const questionTypeSchema = new Schema(
+    {},
+    {
+      discriminatorKey: 'questionType',
+      _id: false,
+    },
+);
+
+const modelSchema = new Schema(
+    {
+      userOwner: {
+        type: Types.ObjectId,
+        ref: COLLECTION_NAME.USER,
+        require: true,
+      },
+      accessAuthorization: {
+        type: Number,
+        require: true,
+        enum: ACCESS_AUTHORIZATION,
+        default: ACCESS_AUTHORIZATION.PUBLIC,
+      },
+      subject: {
+        type: String,
+        require: true,
+      },
+      grade: {
+        type: String,
+        require: true,
+      },
+      topics: {
+        type: Array,
+        default: [],
+      },
+      questionContent: questionTypeSchema,
+    },
+    {
+      timestamps: true,
+      collation: { locale: 'en_US', strength: 1 },
+    },
+);
+
+const choiceQuestion = new Schema(
+    {
+      difficult: {
+        type: Number,
+        enum: ['Nhận biết', 'Thông hiểu', 'Vận dụng'],
+      },
+      contentQuestions: {
+        type: String,
+        require: true,
+      },
+      answerList: [{ answerContent: String, isTrue: { type: Boolean, default: false } }],
+    },
+    {
+      _id: false,
+    },
+);
+
+const multiChoiceQuestion = new Schema(
+    {
+      difficult: {
+        type: Number,
+        enum: ['Nhận biết', 'Thông hiểu', 'Vận dụng'],
+      },
+      contentQuestions: {
+        type: String,
+        require: true,
+      },
+      answerList: [{ answerContent: String, isTrue: { type: Boolean, default: false } }],
+    },
+    {
+      _id: false,
+    },
+);
+
+const textInput = new Schema(
+    {
+      difficult: {
+        type: String,
+        enum: ['Nhận biết', 'Thông hiểu', 'Vận dụng'],
+      },
+      contentQuestions: {
+        type: String,
+        require: true,
+      },
+      answer: {
+        type: String,
+      },
+    },
+    { _id: false },
+);
+
+modelSchema.path('questionContent').discriminator(QUESTION_TYPE.CHOICE, choiceQuestion);
+modelSchema.path('questionContent').discriminator(QUESTION_TYPE.MULTI_CHOICE, multiChoiceQuestion);
+modelSchema.path('questionContent').discriminator(QUESTION_TYPE.TEXT_INPUT, textInput);
+
+const questionModel = model(COLLECTION_NAME.QUESTION, modelSchema);
+
+module.exports = { questionModel };
